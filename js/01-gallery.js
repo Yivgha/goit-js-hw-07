dbb91cbde91eff1c4c89cba0239c1e5ec6aba46f
Yevheniia P <90108689+Yivgha@users.noreply.github.com>
2022-05-09 23:33:19 +0300

    tasks

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+let modalWindow;
+
+const renderGallery = () => {
+    const newGallery = galleryItems
+        .map(({ preview, original, description }) => 
+        `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}"> 
+                <img: {
+                    class = "gallery__image"
+                    data-src = "${original}"
+                    src = "${preview}"
+                    
+                    alt = "${description}"}
+                />
+            </a>
+        </div>
+        `
+    ).join("");
+
+    gallery.innerHTML = newGallery;
+}
+renderGallery();
+
+const onClickGallery = (e) => {
+    e.preventDefault();
+    const getImage = e.target.getAttribute("data-src");
+    if (getImage) {
+        modalWindow = basicLightbox.create(`<div><img src="${getImage} alt="${description}"/></div>`),
+        {
+        onShow: () => gallery.addEventListener("keydown", handleToggle),
+        onClose: () => gallery.addEventListener("keydown", handleToggle)
+        }
+        modalWindow.show();
+    }
+};
+
+const handleToggle = (e) => {
+    if (e.key === "Escape"){
+        modalWindow.close()};
+};
+
+gallery.addEventListener("click", onClickGallery);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const gallery = document.querySelector(".gallery");
+
+const renderGallery = () => {
+    const newGallery = galleryItems
+        .map(({ original, preview, description }) =>
+    `
+        <a class="gallery__item" href="${original}"> 
+            <img: {
+                class ="gallery__image"
+                src = "${preview}"
+                alt = "${description}"}
+            />
+        </a>
+    `
+    ).join("");
+
+    gallery.innerHTML = newGallery;
+    
+}
+renderGallery();
