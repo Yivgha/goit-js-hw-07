69e62387432588ff5585c768b3f2bcc422685ae1
Yevheniia P <90108689+Yivgha@users.noreply.github.com>
2022-05-10 02:39:02 +0300

    task1upd

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,50 +2,40 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const gallery = document.querySelector(".gallery");
 
- const newGallery = galleryItems
-        .map(({ preview, original, description }) => 
-        `
-        <div class="gallery__item">
-            <a class="gallery__link" href="${original}"> 
-                <img: {
-                    class = "gallery__image"
-                    src = "${preview}"
-                    data-src = "${original}"
-                    alt = "${description}"
-                } />
-            </a>
-        </div>
-        `
- ).join("");
+const createGallery = () => {
+    const images = galleryItems
+        .map(
+            ({ original, preview, description }) =>
+                `<li class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" data-source="${original}">
+                </a>
+                </li>`
+        ).join("");
     
-const renderGallery = () => {
-    gallery.insertAdjacentHTML("beforeend", newGallery);
-}
-renderGallery();
+    gallery.innerHTML = images;
+};
+createGallery();
+
 
 
 const onGalleryClick = (e) => {
-    e.preventDefault();
-
-    const getImage = e.target.classList.contains("gallery__link");
-
+    e.prevent.default();
     const handleToggle = (e) => {
-    if (e.key === "Escape"){
-        modalWindow.close();
-        };
-    };
+    if (e.key === "Escape") {
+        modalWindow.close()
+    } 
+}
+    if (e.target === "[data-source]") {
+        const modalWindow = basicLightbox.create(`<img src="${preview}" alt="${description}">`,
+            {  onShow: () => window.addEventListener("keydown", handleToggle),
+                onClose: () => window.addEventListener("keydown", handleToggle),
+            });
+        modalWindow.show();
+    }   
+}
 
-    if (!getImage) {
-        return;
-    }
-    const instanceEl = e.target.dataset.source;
-    const modalWindow = basicLightbox.create((`<div class="gallery__item"><img src="${instanceEl}" alt="e.target.alt"/></div>`),
-        {onClose: () => gallery.addEventListener("keydown", handleToggle),
-            onShow: () => gallery.addEventListener("keydown", handleToggle),
-            
-        });
-    modalWindow.show();
-};
 
 gallery.addEventListener("click", onGalleryClick);
+
 console.log(galleryItems);
