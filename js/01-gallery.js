18c27dbcee8f588f3a05008b242a2d5950b5c8ad
Yevheniia P <90108689+Yivgha@users.noreply.github.com>
2022-05-13 16:45:52 +0300

    task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,41 +1,54 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-const gallery = document.querySelector(".gallery");
 
-const createGallery = () => {
-    const images = galleryItems
-        .map(
-            ({ original, preview, description }) =>
-                `<li class="gallery__item">
-                <a class="gallery__link" href="${original}">
-                <img class="gallery__image" src="${preview}" alt="${description}" data-source="${original}">
-                </a>
-                </li>`
-        ).join("");
-    
-    gallery.innerHTML = images;
+
+//1 Создание и рендер разметки 
+const galleryRef = document.querySelector('.gallery');
+function createMarkup({ preview, original, description}){
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt=""${description}
+    />
+  </a>
+</div>`
 };
-createGallery();
+const markup = galleryItems.map(createMarkup).join('');
+galleryRef.insertAdjacentHTML('afterbegin', markup);
+
+//2.Реализация делегирования
+galleryRef.addEventListener('click', onClickGallery);
+
+function onClickGallery(event){
+    event.preventDefault();
+    if (!event.target.classList.contains("gallery__image")) {
+        return;
+      }
+      createBasicLightbox();
+};
 
 
-
-const onGalleryClick = (e) => {
-    e.prevent.default();
-    const handleToggle = (e) => {
-    if (e.key === "Escape") {
-        modalWindow.close()
-    } 
+function createBasicLightbox(){
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">     
+`,  {
+    onShow: () => {
+    
+      window.addEventListener("keydown", onClickEscape);
+    },
+    onClose: () => window.removeEventListener("keydown", onClickEscape),
+  }
+);
+function onClickEscape(event){
+    if (event.key === "Escape") {
+        instance.close();
+      }
 }
-    if (e.target === "[data-source]") {
-        const modalWindow = basicLightbox.create(`<img src="${preview}" alt="${description}">`,
-            {  onShow: () => window.addEventListener("keydown", handleToggle),
-                onClose: () => window.addEventListener("keydown", handleToggle),
-            });
-        modalWindow.show();
-    }   
-}
-
-
-gallery.addEventListener("click", onGalleryClick);
+instance.show()
+        
+};
 
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,26 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
 
 
 const gallery = document.querySelector(".gallery");
 
-const renderGallery = () => {
-    const newGallery = galleryItems
-        .map(({ original, preview, description }) =>
-    `
-        <a class="gallery__item" href="${original}"> 
-            <img: {
-                class ="gallery__image"
-                src = "${preview}"
-                alt = "${description}"}
-            />
-        </a>
-    `
-    ).join("");
-
-    gallery.innerHTML = newGallery;
+const createGallery = () => {
+    const images = galleryItems
+        .map(
+            ({ original, preview, description }) =>
+                `
+                <a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}">
+                </a>
+                `
+        ).join("");
     
-}
-renderGallery();
+    gallery.innerHTML = images;
+};
+createGallery();
+
+console.log(galleryItems);
